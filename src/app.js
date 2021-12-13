cc0a83d13fc1a5a99a1d42e1d195346d249ea549
Trojan13 <tim.pack@skr.reise>
2021-12-13 16:33:48 +0100

    Added websocket code and server

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 if (module.hot) {
     module.hot.accept();
 }
+const connection = new WebSocket('ws://localhost:8085');
+connection.onerror = error => {
+  console.error(error)
+};
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const lines = ["red", "yellow", "green", "blue"];
@@ -31,4 +35,29 @@ draw();
 
 window.onSliderInput = function onSliderInput(e, slider) {
     return draw();
+};
+
+connection.onmessage = e => {
+
+
+    console.log(e);
+    const d = JSON.parse(e.data);
+    const alpha = toRadian(d.x)
+    const beta = toRadian(d.y)
+    const gamma = toRadian(d.z)
+
+    cube.rotation.x = beta
+    cube.rotation.y = gamma
+    cube.rotation.z = alpha
+    /*const {acceleration, inclination, orientation, pitch, roll, x, y, z} = accelerometer;
+    console.log("Accelerometer:");
+    console.log("  x            : ", x);
+    console.log("  y            : ", y);
+    console.log("  z            : ", z);
+    console.log("  pitch        : ", pitch);
+    console.log("  roll         : ", roll);
+    console.log("  acceleration : ", acceleration);
+    console.log("  inclination  : ", inclination);
+    console.log("  orientation  : ", orientation);
+    console.log("--------------------------------------");*/
 };
