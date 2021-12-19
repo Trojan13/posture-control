0ecb9970d411cff20d3bd8e4609e51a9b953b16a
Trojan13 <tim.pack@skr.reise>
2021-12-19 15:44:11 +0100

    Removed wsStatus

--- esp8266/clients/client_wifi/client_wifi.ino
+++ esp8266/clients/client_wifi/client_wifi.ino
@@ -16,7 +16,6 @@ WebSocketsClient ws_client;
 Adafruit_MPU6050 mpu_1, mpu_2;
 Adafruit_Sensor *mpu_gyro_1, *mpu_gyro_2, *mpu_accel_1, *mpu_accel_2;
 bool sensorsSetup;
-int wsStatus = 0;
 
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 {
@@ -24,21 +23,18 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length)
   switch (type)
   {
   case WStype_DISCONNECTED:
-    Serial.printf("[WSc] Disconnected!\n");
-    wsStatus = 0;
+    Serial.printf("Disconnected!\n");
     break;
   case WStype_CONNECTED:
   {
-    Serial.printf("[WSc] Connected!\n");
-
-    wsStatus = 1;
+    Serial.printf("Connected!\n");
   }
   break;
   case WStype_TEXT:
-    Serial.printf("[WSc] get text: %s\n", payload);
+    Serial.printf("Get text: %s\n", payload);
     break;
   case WStype_BIN:
-    Serial.printf("[WSc] get binary length: %u\n", length);
+    Serial.printf("Get binary length: %u\n", length);
     hexdump(payload, length);
     break;
   }
@@ -127,47 +123,42 @@ void setupSensors()
 void loop()
 {
   ws_client.loop();
-  Serial.println(wsStatus);
+  sensors_event_t gyro_1;
+  sensors_event_t gyro_2;
+  sensors_event_t accel_1;
+  sensors_event_t accel_2;
 
-  if (sensorsSetup && wsStatus == 1)
-  {
-    sensors_event_t gyro_1;
-    sensors_event_t gyro_2;
-    sensors_event_t accel_1;
-    sensors_event_t accel_2;
+  mpu_gyro_1->getEvent(&gyro_1);
+  mpu_gyro_2->getEvent(&gyro_2);
 
-    mpu_gyro_1->getEvent(&gyro_1);
-    mpu_gyro_2->getEvent(&gyro_2);
+  mpu_accel_1->getEvent(&accel_1);
+  mpu_accel_2->getEvent(&accel_2);
 
-    mpu_accel_1->getEvent(&accel_1);
-    mpu_accel_2->getEvent(&accel_2);
+  Serial.print(gyro_1.gyro.y);
+  Serial.print(",");
+  Serial.print(gyro_2.gyro.y);
+  Serial.println();
 
-    Serial.print(gyro_1.gyro.y);
-    Serial.print(",");
-    Serial.print(gyro_2.gyro.y);
-    Serial.println();
+  DynamicJsonDocument doc(1024);
 
-    DynamicJsonDocument doc(1024);
+  doc["ws_client"] = client_name;
+  doc["mpu_1"]["gyro"]["x"] = gyro_1.gyro.x;
+  doc["mpu_1"]["gyro"]["y"] = gyro_1.gyro.y;
+  doc["mpu_1"]["gyro"]["z"] = gyro_1.gyro.z;
+  doc["mpu_1"]["accel"]["x"] = accel_1.acceleration.x;
+  doc["mpu_1"]["accel"]["y"] = accel_1.acceleration.y;
+  doc["mpu_1"]["accel"]["z"] = accel_1.acceleration.z;
 
-    doc["ws_client"] = client_name;
-    doc["mpu_1"]["gyro"]["x"] = gyro_1.gyro.x;
-    doc["mpu_1"]["gyro"]["y"] = gyro_1.gyro.y;
-    doc["mpu_1"]["gyro"]["z"] = gyro_1.gyro.z;
-    doc["mpu_1"]["accel"]["x"] = accel_1.acceleration.x;
-    doc["mpu_1"]["accel"]["y"] = accel_1.acceleration.y;
-    doc["mpu_1"]["accel"]["z"] = accel_1.acceleration.z;
+  doc["mpu_2"]["gyro"]["x"] = gyro_2.gyro.x;
+  doc["mpu_2"]["gyro"]["y"] = gyro_2.gyro.y;
+  doc["mpu_2"]["gyro"]["z"] = gyro_2.gyro.z;
+  doc["mpu_2"]["accel"]["x"] = accel_2.acceleration.x;
+  doc["mpu_2"]["accel"]["y"] = accel_2.acceleration.y;
+  doc["mpu_2"]["accel"]["z"] = accel_2.acceleration.z;
 
-    doc["mpu_2"]["gyro"]["x"] = gyro_2.gyro.x;
-    doc["mpu_2"]["gyro"]["y"] = gyro_2.gyro.y;
-    doc["mpu_2"]["gyro"]["z"] = gyro_2.gyro.z;
-    doc["mpu_2"]["accel"]["x"] = accel_2.acceleration.x;
-    doc["mpu_2"]["accel"]["y"] = accel_2.acceleration.y;
-    doc["mpu_2"]["accel"]["z"] = accel_2.acceleration.z;
-
-    String output;
-    serializeJson(doc, output);
-    ws_client.sendTXT(output);
-    Serial.println(output);
-  }
+  String output;
+  serializeJson(doc, output);
+  ws_client.sendTXT(output);
+  Serial.println(output);
   delay(1000);
 }
