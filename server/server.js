85b1b1ae878d921943ee9b0f392fb5bc97a8f323
Trojan13 <tim_colin.pack1@smail.fh-koeln.de>
2021-12-30 20:04:41 +0100

    Formatted server

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const WebSocket = require('ws')
 const readLineParser = new SerialPort.parsers.Readline();
 const wss = new WebSocket.Server({ port: 8085 });
 
-SerialPort.list().then((ports)=> {
+SerialPort.list().then((ports) => {
   ports.forEach(function (port) {
     console.log(port);
   });
@@ -15,13 +15,13 @@ let port = new SerialPort("COM6", {
 
 
 
-  wss.on('connection', ws => {
-    ws.on('message', message => {
-      console.log(`Received message => ${message}`)
-    })
-    
-readLineParser.on('data', (data)=>{
-  ws.send(JSON.stringify(data));
-  console.log(data);
+wss.on('connection', ws => {
+  ws.on('message', message => {
+    console.log(`Received message => ${message}`)
+  })
+
+  readLineParser.on('data', (data) => {
+    ws.send(JSON.stringify(data));
+    console.log(data);
   });
-  });
+});
