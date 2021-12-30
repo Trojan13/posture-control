5b285a40ec4878c96890ddb753370f60402524ae
Trojan13 <tim.pack@skr.reise>
2021-12-30 20:04:12 +0100

    Server works now

--- server/server.js
+++ server/server.js
@@ -1,45 +1,27 @@
 const SerialPort = require("serialport");
+const WebSocket = require('ws')
 
-SerialPort.list(function (err, ports) {
+const readLineParser = new SerialPort.parsers.Readline();
+const wss = new WebSocket.Server({ port: 8085 });
+
+SerialPort.list().then((ports)=> {
   ports.forEach(function (port) {
-    console.log(port.comName, port.pnpId, port.manufacturer); // or console.log(port)
+    console.log(port);
   });
 });
-let port = new SerialPort("COM5", {
+let port = new SerialPort("COM6", {
   baudRate: 115200
 });
 
-port.on('open', function () {
-  console.log('Serial Port Connected...');
-});
 
-let buffer = '';
-port.on('data', function (chunk) {
-  buffer += chunk;
-  var answers = buffer.split(/\r?\n/);
-  buffer = answers.pop();
-
-  if (answer.length > 0)
-    console.log(answer[0]);
-});
-
-port.on('error', function (err) {
-  console.log(err);
-});
-
-/*
-  const WebSocket = require('ws')
-  const wss = new WebSocket.Server({ port: 8085 })
 
   wss.on('connection', ws => {
     ws.on('message', message => {
       console.log(`Received message => ${message}`)
     })
-
-    //http://johnny-five.io/examples/imu-mpu6050/
-    imu.on("change", () => {
-      const d = { x: imu.gyro.pitch, y: imu.gyro.roll, z: imu.gyro.yaw };
-      ws.send(JSON.stringify(d));
-    });
+    
+readLineParser.on('data', (data)=>{
+  ws.send(JSON.stringify(data));
+  console.log(data);
   });
-  */
+  });
