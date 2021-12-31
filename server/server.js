a5a093cdd42df12d9b41f089aeef5925c333d42d
Trojan13 <tim_colin.pack1@smail.fh-koeln.de>
2021-12-31 11:46:02 +0100

    Adjusted server dto

--- server/server.js
+++ server/server.js
@@ -4,9 +4,14 @@ const WebSocket = require('ws');
 const readLineParser = new SerialPort.parsers.Readline();
 const wss = new WebSocket.Server({ port: 8085 });
 
-let pressureArray = [];
-let anglesArray1 = [];
-let anglesArray2 = [];
+let sensorDataObject = {
+  angle1: null,
+  angle2: null,
+  angle3: null,
+  angle4: null,
+  pressure1: null,
+  pressure2: null,
+};
 
 SerialPort.list().then((ports) => {
   ports.forEach(function (port) {
@@ -34,17 +39,19 @@ wss.on('connection', ws => {
   readLineParser.on('data', (data) => {
     const comPortdataObject = JSON.parse(data);
     if (comPortdataObject.ws_client === "client_3") {
-      pressureArray = [comPortdataObject.fsr_1,comPortdataObject.fsr_2];
+      sensorDataObject.pressure1 = comPortdataObject.fsr_1;
+      sensorDataObject.pressure2 = comPortdataObject.fsr_2;
     }
-    if (comPortdataObject.ws_client === "client_1" ) {
-      anglesArray1 = [comPortdataObject.mpu_1.accel.y,comPortdataObject.mpu_2.accel.y];
+    if (comPortdataObject.ws_client === "client_1") {
+      sensorDataObject.angle1 = comPortdataObject.mpu_1.accel.y;
+      sensorDataObject.angle2 = comPortdataObject.mpu_2.accel.y;
     }
-    if (comPortdataObject.ws_client === "client_2" ) {
-      anglesArray2 = [comPortdataObject.mpu_1.accel.y,comPortdataObject.mpu_2.accel.y];
+    if (comPortdataObject.ws_client === "client_2") {
+      sensorDataObject.angle3 = comPortdataObject.mpu_1.accel.y;
+      sensorDataObject.angle4 = comPortdataObject.mpu_2.accel.y;
     }
-    if(pressureArray && anglesArray1 && anglesArray2) {
-    ws.send(JSON.stringify({angle}));
-      
+    if (sensorDataObject.pressure1 && sensorDataObject.pressure2 && sensorDataObject.angle1 && sensorDataObject.angle2 && sensorDataObject.angle3 && sensorDataObject.angle4) {
+      ws.send(JSON.stringify({ sensorDataObject }));
     }
     console.log(data);
   });
