f0a1df3c247dc2cd5b3b448f82630ed08f88dc8f
Trojan13 <tim_colin.pack1@smail.fh-koeln.de>
2021-12-31 11:32:10 +0100

    Updated frontend

--- src/app.js
+++ src/app.js
@@ -1,17 +1,62 @@
 if (module.hot) {
     module.hot.accept();
 }
-const connection = new WebSocket('ws://localhost:8085');
-connection.onerror = error => {
-    console.error(error)
-};
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const lines = ["red", "yellow", "green", "blue"];
 const start = [200, 400];
 const lineLength = 100;
 
-function draw(wsAngles = null) {
+let image = new Image();
+image.src = new URL(
+    './assets/foot.png',
+    import.meta.url
+);
+image.onload = () => {
+    drawFoot(ctx);
+};
+
+function connect() {
+    const connection = new WebSocket('ws://localhost:8085');
+    connection.onopen = function () {
+        // subscribe to some channels
+    };
+
+    connection.onmessage = function (e) {
+        const d = JSON.parse(e.data);
+        draw([d.angle1, d.angle2, d.angle3, d.angle4], [d.pressure1, d.pressure2]);
+    };
+
+    connection.onclose = function (e) {
+        console.log('Socket is closed. Reconnect will be attempted in 10 seconds.', e.reason);
+        setTimeout(function () {
+            connect();
+        }, 10000);
+    };
+
+    connection.onerror = function (err) {
+        console.error('Socket encountered error: ', err.message, 'Closing socket');
+        connection.close();
+    };
+}
+
+
+function drawCircle(ctx, pressure1 = 0, pressure2 = 0) {
+    const radius1 = 5 + 30 / 400 * pressure1;
+    const radius2 = 5 + 30 / 400 * pressure2;
+    ctx.beginPath();
+    ctx.arc(60, 90, radius1, 0, 2 * Math.PI, false);
+    ctx.arc(55, 180, radius2, 0, 2 * Math.PI, false);
+    ctx.fillStyle = 'green';
+    ctx.fill();
+}
+
+function drawFoot(ctx) {
+    ctx.drawImage(image, 10, 10, 100, 200);
+}
+
+function draw(wsAngles = null, wsPressure = null) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let prev = start;
     for (let i = 0; i < lines.length; i++) {
@@ -31,15 +76,13 @@ function draw(wsAngles = null) {
         ctx.stroke();
         ctx.closePath();
     }
+    drawCircle(ctx, wsPressure ? wsPressure[0] : 0, wsPressure ? wsPressure[1] : 0);
+    drawFoot(ctx);
 }
-draw();
 
 window.onSliderInput = function onSliderInput(e, slider) {
     return draw();
 };
 
-connection.onmessage = e => {
-    const d = JSON.parse(e.data);
-    const dy = d.y;
-    draw([dy,0,0,0]);
-};
+draw();
+connect();
