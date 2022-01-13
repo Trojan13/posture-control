d303e2dbab8bce4baf9621cdf59a40936212e036
Trojan13 <tim_colin.pack1@smail.fh-koeln.de>
2022-01-13 10:28:09 +0100

    Updated mpu error

--- esp8266/client_mpu_error/client_mpu_error.ino
+++ esp8266/client_mpu_error/client_mpu_error.ino
@@ -41,16 +41,6 @@ void calculate_IMU_error(int MPU)
   Wire.write(0x6B);            // Talk to the register 6B
   Wire.write(0x00);            // Make reset - place a 0 into the 6B register
   Wire.endTransmission(true);  //end the transmission
-  // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1C); //Talk to the ACCEL_CONFIG register (1C hex)
-  Wire.write(0x10); //Set the register bits as 00010000 (+/- 8g full scale range)
-  Wire.endTransmission(true);
-  // Configure Gyro Sensitivity - Full Scale Range (default +/- 250deg/s)
-  Wire.beginTransmission(MPU);
-  Wire.write(0x1B); // Talk to the GYRO_CONFIG register (1B hex)
-  Wire.write(0x10); // Set the register bits as 00010000 (1000deg/s full scale)
-  Wire.endTransmission(true);
   delay(20);
   // Call this function if you need to get the IMU error values for your module
   // We can call this funtion in the setup section to calculate the accelerometer and gyro data error. From here we will get the error values used in the above equations printed on the Serial Monitor.
