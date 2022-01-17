8523b644d563fa42f9374ba08f47f6db0ce5b558
Trojan13 <tim_colin.pack1@smail.fh-koeln.de>
2022-01-17 17:47:49 +0100

    New test

---
+++ esp8266/client_mpu_two/client_mpu_two.ino
@@ -0,0 +1,275 @@
+/* ============================================
+  I2Cdev device library code is placed under the MIT license
+  Copyright (c) 2021 Homer Creutz
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+  ===============================================
+*/
+
+/* 2 MPUXXXX Example
+ *  These can be any MPU MPU6050, MPU6500, MPU9150, MPU9155, MPU9250 ETC...
+ *  Attach 2 MPU's to the I2C buss
+ *  Power both MPU's According to specs. Generic Breakout Version Powers with 5V and has a onboard Voltage regulator.
+ *  attach a 2K ohm resister between Pin 8 and AD0 on one of the MPU's
+ *  attach a 2K ohm resister between Pin 9 and AD0 on one of the other MPU
+ *  Sketch will load both MPU's with DMP Firmware
+ *  Interrupt pin is not connected.
+ */
+
+
+//#include "MPU_ReadMacros.h"
+//#inc;ude "MPU_WriteMacros.h"
+#include "Simple_MPU6050.h"
+#define MPU6050_ADDRESS_AD0_LOW     0x68 // address pin low (GND), default for InvenSense evaluation board
+#define MPU6050_ADDRESS_AD0_HIGH    0x69 // address pin high (VCC)
+#define MPU6050_DEFAULT_ADDRESS     MPU6050_ADDRESS_AD0_LOW
+
+Simple_MPU6050 mpu;
+/*             _________________________________________________________*/
+//               X Accel  Y Accel  Z Accel   X Gyro   Y Gyro   Z Gyro
+//#define OFFSETS  -5260,    6596,    7866,     -45,       5,      -9  // My Last offsets.
+//       You will want to use your own as these are only for my specific MPU6050.
+/*             _________________________________________________________*/
+
+//***************************************************************************************
+//******************                Print Funcitons                **********************
+//***************************************************************************************
+
+#define spamtimer(t) for (static uint32_t SpamTimer; (uint32_t)(millis() - SpamTimer) >= (t); SpamTimer = millis()) // (BLACK BOX) Ya, don't complain that I used "for(;;){}" instead of "if(){}" for my Blink Without Delay Timer macro. It works nicely!!!
+
+/* printfloatx() is a helper Macro used with the Serial class to simplify my code and provide enhanced viewing of Float and interger values:
+   usage: printfloatx(Name,Variable,Spaces,Precision,EndTxt);
+   Name and EndTxt are just char arrays
+   Variable is any numerical value byte, int, long and float
+   Spaces is the number of spaces the floating point number could possibly take up including +- and decimal point.
+   Percision is the number of digits after the decimal point set to zero for intergers
+*/
+
+#define printfloatx(Name,Variable,Spaces,Precision,EndTxt) print(Name); {char S[(Spaces + Precision + 3)];Serial.print(F(" ")); Serial.print(dtostrf((float)Variable,Spaces,Precision ,S));}Serial.print(EndTxt);//printfloatx(Name,Variable,Spaces,Precision,EndTxt)
+
+int PrintValues(int32_t *quat, uint16_t SpamDelay = 100) {
+  uint8_t WhoAmI;
+  Quaternion q;
+  VectorFloat gravity;
+  float ypr[3] = { 0, 0, 0 };
+  float xyz[3] = { 0, 0, 0 };
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.WHO_AM_I_READ_WHOAMI(&WhoAmI);
+    Serial.print(F("WhoAmI= 0x"));
+    Serial.print(WhoAmI, HEX);
+    Serial.print(" ");
+    mpu.GetQuaternion(&q, quat);
+    mpu.GetGravity(&gravity, &q);
+    mpu.GetYawPitchRoll(ypr, &q, &gravity);
+    mpu.ConvertToDegrees(ypr, xyz);
+    Serial.printfloatx(F("Yaw")  , xyz[0], 9, 4, F(", ")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx(F("Pitch"), xyz[1], 9, 4, F(", "));
+    Serial.printfloatx(F("Roll") , xyz[2], 9, 4, F("\n"));
+  }
+}
+
+int ChartValues(int32_t *quat, uint16_t SpamDelay = 100) {
+  Quaternion q;
+  VectorFloat gravity;
+  float ypr[3] = { 0, 0, 0 };
+  float xyz[3] = { 0, 0, 0 };
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.GetQuaternion(&q, quat);
+    mpu.GetGravity(&gravity, &q);
+    mpu.GetYawPitchRoll(ypr, &q, &gravity);
+    mpu.ConvertToDegrees(ypr, xyz);
+    Serial.printfloatx("", xyz[0], 9, 4, F(",")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx("", xyz[1], 9, 4, F(","));
+    Serial.printfloatx("", xyz[2], 9, 4, F("\n"));
+  }
+}
+
+//Gyro, Accel and Quaternion
+int PrintAllValues(int16_t *gyro, int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
+  Quaternion q;
+  VectorFloat gravity;
+  float ypr[3] = { 0, 0, 0 };
+  float xyz[3] = { 0, 0, 0 };
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.GetQuaternion(&q, quat);
+    mpu.GetGravity(&gravity, &q);
+    mpu.GetYawPitchRoll(ypr, &q, &gravity);
+    mpu.ConvertToDegrees(ypr, xyz);
+    Serial.printfloatx(F("Yaw")  , xyz[0], 9, 4, F(",   ")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx(F("Pitch"), xyz[1], 9, 4, F(",   "));
+    Serial.printfloatx(F("Roll") , xyz[2], 9, 4, F(",   "));
+    Serial.printfloatx(F("ax")   , accel[0], 5, 0, F(",   "));
+    Serial.printfloatx(F("ay")   , accel[1], 5, 0, F(",   "));
+    Serial.printfloatx(F("az")   , accel[2], 5, 0, F(",   "));
+    Serial.printfloatx(F("gx")   , gyro[0],  5, 0, F(",   "));
+    Serial.printfloatx(F("gy")   , gyro[1],  5, 0, F(",   "));
+    Serial.printfloatx(F("gz")   , gyro[2],  5, 0, F("\n"));
+    Serial.println();
+  }
+}
+
+int ChartAllValues(int16_t *gyro, int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
+  Quaternion q;
+  VectorFloat gravity;
+  float ypr[3] = { 0, 0, 0 };
+  float xyz[3] = { 0, 0, 0 };
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.GetQuaternion(&q, quat);
+    mpu.GetGravity(&gravity, &q);
+    mpu.GetYawPitchRoll(ypr, &q, &gravity);
+    mpu.ConvertToDegrees(ypr, xyz);
+    Serial.printfloatx("", xyz[0], 9, 4, F(",")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx("", xyz[1], 9, 4, F(","));
+    Serial.printfloatx("", xyz[2], 9, 4, F(", "));
+    Serial.printfloatx("", accel[0], 5, 0, F(","));
+    Serial.printfloatx("", accel[1], 5, 0, F(","));
+    Serial.printfloatx("", accel[2], 5, 0, F(","));
+    Serial.printfloatx("", gyro[0],  5, 0, F(","));
+    Serial.printfloatx("", gyro[1],  5, 0, F(","));
+    Serial.printfloatx("", gyro[2],  5, 0, F("\n"));
+  }
+}
+
+int PrintQuaternion(int32_t *quat, uint16_t SpamDelay = 100) {
+  Quaternion q;
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.GetQuaternion(&q, quat);
+    Serial.printfloatx(F("quat w")  , q.w, 9, 4, F(",   ")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx(F("x")       , q.x, 9, 4, F(",   "));
+    Serial.printfloatx(F("y")       , q.y, 9, 4, F(",   "));
+    Serial.printfloatx(F("z")       , q.z, 9, 4, F("\n"));
+  }
+}
+
+int PrintEuler(int32_t *quat, uint16_t SpamDelay = 100) {
+  Quaternion q;
+  float euler[3];         // [psi, theta, phi]    Euler angle container
+  float eulerDEG[3];         // [psi, theta, phi]    Euler angle container
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.GetQuaternion(&q, quat);
+    mpu.GetEuler(euler, &q);
+    mpu.ConvertToDegrees(euler, eulerDEG);
+    Serial.printfloatx(F("euler  ")  , eulerDEG[0], 9, 4, F(",   ")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx(F("")       , eulerDEG[1], 9, 4, F(",   "));
+    Serial.printfloatx(F("")       , eulerDEG[2], 9, 4, F("\n"));
+  }
+}
+
+int PrintRealAccel(int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
+  Quaternion q;
+  VectorFloat gravity;
+  VectorInt16 aa, aaReal;
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.GetQuaternion(&q, quat);
+    mpu.GetGravity(&gravity, &q);
+    mpu.SetAccel(&aa, accel);
+    mpu.GetLinearAccel(&aaReal, &aa, &gravity);
+    Serial.printfloatx(F("aReal x")  , aaReal.x , 9, 4, F(",   ")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx(F("y")        , aaReal.y , 9, 4, F(",   "));
+    Serial.printfloatx(F("z")        , aaReal.z, 9, 4, F("\n"));
+  }
+}
+
+
+int PrintWorldAccel(int16_t *accel, int32_t *quat, uint16_t SpamDelay = 100) {
+  Quaternion q;
+  VectorFloat gravity;
+  VectorInt16 aa, aaReal, aaWorld;
+  spamtimer(SpamDelay) {// non blocking delay before printing again. This skips the following code when delay time (ms) hasn't been met
+    mpu.GetQuaternion(&q, quat);
+    mpu.GetGravity(&gravity, &q);
+    mpu.SetAccel(&aa, accel);
+    mpu.GetLinearAccel(&aaReal, &aa, &gravity);
+    mpu.GetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+    Serial.printfloatx(F("aWorld x")  , aaWorld.x , 9, 4, F(",   ")); //printfloatx is a Helper Macro that works with Serial.print that I created (See #define above)
+    Serial.printfloatx(F("y")        , aaWorld.y, 9, 4, F(",   "));
+    Serial.printfloatx(F("z")        , aaWorld.z, 9, 4, F("\n"));
+  }
+}
+//***************************************************************************************
+//******************              Callback Funciton                **********************
+//***************************************************************************************
+
+
+void print_Values (int16_t *gyro, int16_t *accel, int32_t *quat, uint32_t *timestamp) {
+  uint8_t Spam_Delay = 10; // Built in Blink without delay timer preventing Serial.print SPAM
+  PrintValues(quat, Spam_Delay);
+  // ChartValues(quat, Spam_Delay);
+  // PrintAllValues(gyro, accel, quat, Spam_Delay);
+  // ChartAllValues(gyro, accel, quat, Spam_Delay);
+  // PrintQuaternion(quat, Spam_Delay);
+  // PrintEuler(quat, Spam_Delay);
+  // PrintRealAccel(accel, quat,  Spam_Delay);
+  // PrintWorldAccel(accel, quat, Spam_Delay);
+}
+
+//***************************************************************************************
+//******************                Setup and Loop                 **********************
+//***************************************************************************************
+/*
+   #define DMP_200Hz  0x00, 0x00
+   #define DMP_100Hz  0x00, 0x01
+   #define DMP_50Hz  0x00, 0x03
+   #define DMP_40Hz 0x00, 0x04
+   #define DMP_25Hz 0x00, 0x07
+   #define DMP_20Hz 0x00, 0x09
+   #define DMP_10Hz 0x00, 0x13
+   #define DMP_5Hz  0x00, 0x27
+   #define DMP_1Hz 0x00, 0xC7
+   #define DMP_1sec 0x00, 0xC7
+   #define DMP_10sec  0x07, 0xCF
+   #define DMP_60sec  0x2E, 0xDF
+*/
+void setup() {
+
+  uint8_t val;
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+#ifdef __AVR__  
+  Wire.setWireTimeout(3000, true); //timeout value in uSec
+#endif
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
+  // initialize serial communication
+  Serial.begin(115200);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+  Serial.println(F("Start:"));
+  mpu.SetAddress(MPU6050_DEFAULT_ADDRESS)
+    mpu.Set_DMP_Output_Rate_Hz(10);           // Set the DMP output rate from 200Hz to 5 Minutes.
+  //mpu.Set_DMP_Output_Rate_Seconds(10);   // Set the DMP output rate in Seconds
+  //mpu.Set_DMP_Output_Rate_Minutes(5);    // Set the DMP output rate in Minute
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  mpu.CalibrateMPU().load_DMP_Image();// Does it all for you with Calibration
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  mpu.CalibrateMPU().Enable_Reload_of_DMP().load_DMP_Image();// Does it all for you with Calibration
+  mpu.on_FIFO(print_Values);
+}
+
+void loop() {
+  digitalWrite(8, !digitalRead(8));
+  digitalWrite(9, !digitalRead(9));
+  mpu.dmp_read_fifo(0);// Must be in loop
+}
